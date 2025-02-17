ec7af22bfb8d5a2ae11c4eb1a83dde2e685e2d4f
Md:Mosaraf Hossen <151431976+mosaraf68500@users.noreply.github.com>
2025-02-18 00:30:47 +0600

    complete

--- problem-1.js
+++ problem-1.js
@@ -1,1 +1,12 @@
-const a=5;
+function calculateVAT( price ) {
+    if (typeof price !== "number" || isNaN(price) || price <= 0) {
+        return "Invalid";
+    }
+    const total_vat=price*7.5;
+    const vat_result=total_vat/100;
+    return vat_result;
+}
+
+
+const result=calculateVAT(999)
+console.log(result)
